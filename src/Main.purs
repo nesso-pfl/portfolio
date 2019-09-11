58c608b5d174d31419006a0ff010a3b304e47885
shugoooi <ghbjy6@gmail.com>
2019-09-11 18:12:45 +0900

    save routing

--- src/Main.purs
+++ src/Main.purs
@@ -3,14 +3,23 @@ module Main where
 import Router as R
 
 import Prelude
+import Foreign (unsafeToForeign)
 import Effect (Effect)
+import Effect.Console (log)
 import Effect.Aff (forkAff)
+import Effect.Class (liftEffect)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Routing.PushState as RP
 
 
 main :: Effect Unit
 main = HA.runHalogenAff do
     body <- HA.awaitBody
-    driver <- runUI R.ui unit body
-    forkAff $ R.routeSignal driver
+    -- runUI R.ui unit body
+    psi <- liftEffect RP.makeInterface
+    ls <- liftEffect psi.locationState
+    driver <- runUI R.ui ls.path body
+    _ <- liftEffect $ psi.listen (R.listen driver)
+    liftEffect $ log ls.path
+    -- forkAff $ R.routeSignal driver
--- src/Router.purs
+++ src/Router.purs
@@ -5,7 +5,7 @@ import Blog as Blog
 
 import Control.Alt ((<|>))
 import Data.Const (Const)
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff)
@@ -17,16 +17,17 @@ import Halogen.HTML.Properties as HP
 import Prelude
 import Routing.Hash (matches)
 import Routing.Match (Match, lit)
-
-data Input a
-    = Goto Routes a
+import Routing.PushState (LocationState)
 
 data Routes
     = Home
     | Blog
 
-data Query
+data Action
     = ChangeRoute Routes
+
+data Query a 
+    = PushRoute LocationState a
 
 type Slot =
     ( home :: Home.Slot Unit
@@ -40,10 +41,11 @@ type State =
     { currentPage :: String
     }
 
+type Input = String
 
-initialState :: forall i. i -> State
-initialState _ =
-    { currentPage: "Home"
+initialState :: Input -> State
+initialState i =
+    { currentPage: i
     }
 
 routing :: Match Routes
@@ -51,44 +53,53 @@ routing = Home <$ lit ""
       <|> Blog <$ lit "" <* lit "blog"
 
 
-ui :: forall q. H.Component HH.HTML q Unit Unit Aff
+ui :: H.Component HH.HTML Query Input Unit Aff
 ui = H.mkComponent
     { initialState
     , render
     , eval: H.mkEval $ H.defaultEval
-        { handleQuery = handleQuery
+        { handleAction = handleAction
+        , handleQuery = handleQuery
         }
     }
 
 
-render :: State -> H.ComponentHTML Unit Slot Aff
+render :: State -> H.ComponentHTML Action Slot Aff
 render st =
     HH.div_
         [ HH.h1_ [ HH.text $ "どうも、" <> st.currentPage ]
-        , HH.a [ HP.href "/home"] [ HH.text "リンク" ]
+        , HH.div_ [ HH.a [ HP.href "/home"] [ HH.text "to home" ] ]
+        , HH.div_ [ HH.a [ HP.href "/blog"] [ HH.text "to blog" ] ]
         , view st.currentPage
         ]
 
     where
-        view :: String -> H.ComponentHTML Unit Slot Aff
+        view :: String -> H.ComponentHTML Action Slot Aff
         view "home" = HH.slot _home unit Home.ui unit absurd
         view "blog" = HH.slot _blog unit Blog.ui unit absurd
         view _ = HH.div_ []
 
--- handleQuery :: forall a. query a -> HalogenM state action slots output m (Maybe a)
-handleQuery :: forall a. Query a -> H.HalogenM State Query Slot Unit Aff (Maybe a)
-handleQuery = case _ of
+handleAction :: Action -> H.HalogenM State Action Slot Unit Aff Unit
+handleAction = case _ of
     ChangeRoute Home -> do
        H.modify_ (_ { currentPage = "home" })
     ChangeRoute Blog -> do
        H.modify_ (_ { currentPage = "blog" })
 
--- routeSignal :: H.HalogenIO (Const Unit) Void Aff -> Aff (Effect Unit)
-routeSignal :: H.HalogenIO Input Unit Aff -> Aff (Effect Unit)
-routeSignal driver = liftEffect do
-    matches routing hashChanged
-    where
-        hashChanged _ newRoute = do
-           launchAff $ driver.query <<< H.tell <<< Goto $ newRoute
-           pure unit
+handleQuery :: forall a. Query a -> H.HalogenM State Action Slot Unit Aff (Maybe a)
+handleQuery = case _ of
+    PushRoute ls a -> do
+        H.modify_ (_ { currentPage = ls.path })
+        pure unit
 
+listen driver ls = do
+    _ <- launchAff $ driver.query $ H.tell (PushRoute ls)
+    pure unit
+-- routeSignal :: H.HalogenIO (Const Unit) Void Aff -> Aff (Effect Unit)
+-- routeSignal :: H.HalogenIO Input Unit Aff -> Aff (Effect Unit)
+-- routeSignal driver = liftEffect do
+    -- matches routing hashChanged
+    -- where
+        -- hashChanged _ newRoute = do
+           -- launchAff $ driver.query <<< H.tell <<< Goto $ newRoute
+           -- pure unit
