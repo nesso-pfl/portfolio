ab6f164c3704f01c1bd4d75a7367d7599b4ad9b9
nesso <32949286+nesso-pfl@users.noreply.github.com>
2020-06-27 17:10:47 +0900

    Add profile on Home page

--- src/pages/Home.purs
+++ src/pages/Home.purs
@@ -5,7 +5,7 @@ import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Halogen as H
 import Halogen.HTML as HH
-import Plugin.HalogenR (setTitle)
+import Plugin.HalogenR (divC1, setTitle)
 import Prelude
 
 
@@ -27,8 +27,17 @@ type Slot = H.Slot (Const Unit) Void
 render :: Unit -> H.ComponentHTML Action () Aff
 render st =
     HH.div_
-        [ HH.h1_ [ HH.text "Home" ]
-        , HH.h1_ [ HH.text $ show st]
+        [ HH.h2_ [ HH.text "nesso Profile"]
+        , HH.p_ [ HH.text "主にフロントエンドエンジニアを生業にしているが、一応バックエンドや AWS でのインフラ構築も見ることができる。" ]
+        , HH.p_
+            [ HH.text $ "静的型付き言語・関数型言語に強い関心がある。一方で動的型付き言語や、"
+                     <> "関数型の流儀で記述するための機能に乏しい言語はあまり触りたがらない。"
+            ]
+        , HH.p_
+            [ HH.text $ "最近では Rust で WebSocket サーバーを立てようとして挫折したり、webpack のソースコードを完全に理解しようとして挫折したり、"
+                     <> "TaPL を完全に理解しようとして挫折している。"
+            ]
+        , HH.p_ [ HH.text $ "GitHub: https://github.com/nesso-pfl" ]
         ]
 
 handleAction :: Action -> H.HalogenM Unit Action () Void Aff Unit
